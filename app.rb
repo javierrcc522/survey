6a904cf4c50e22602259f6e0efbb889ed48671b3
javier jimenez and sara spink <javier@>
2017-09-26 15:11:59 -0700

    add an update option to survey

--- app.rb
+++ app.rb
@@ -17,3 +17,18 @@ post("/survey/new") do
   @survey = Survey.all
   erb(:index)
 end
+
+get("/survey/new/:id") do
+  @survey = Survey.find(params[:id])
+  @question = Question.all()
+  erb(:survey_info)
+end
+
+patch("/survey/:id") do
+  @survey = Survey.find(params[:id])
+  description = params['description']
+  @survey.update({:description => description})
+
+
+  erb(:survey_info)
+end
